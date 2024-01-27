e67ff08eaa261e5f21dbe7f3dbf8ec34448d0808
Sundaravel <sundaravel.exe@gmail.com>
2024-01-27 19:15:38 +0530

    hash the password

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -5,13 +5,23 @@ const { MongooseError } = require('mongoose');
 
 exports.register = async (req, res) => {
     try {
-        const { email } = req.body;
+        const { fullName, email, password, registrationNumber, phoneNumber, department, batch } = req.body;
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).send('Email already exists');
         }
-        const user = await User.create(req.body);
-        res.send('User registered successfully!');
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log('hashpass',hashedPassword);
+        const user = await User.create({
+          fullName,
+          email,
+          password: hashedPassword,
+          registrationNumber,
+          phoneNumber,
+          department,
+          batch
+      });
+      res.send('User registered successfully!');
     } catch (err) {
         console.error("Registration Error:", err); 
         res.status(400).send(`Registration failed: ${err.message}`);
