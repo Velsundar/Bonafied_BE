d569cc11440c2cbf9545a6d361f67038fb1aade2
Sundaravel <sundaravel.exe@gmail.com>
2024-02-02 16:47:08 +0530

    Bonafied Model and controller for request set

---
+++ Controllers/BonafiedController.js
@@ -0,0 +1,31 @@
+const Bonafied = require("../Models/ApplyBonafiedmodel");
+
+exports.getAll = async (req, res) => {
+  try {
+    const bonafiedList = await Bonafied.find();
+    res.status(200).json(bonafiedList);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+exports.getById = async (req, res) => {
+  try {
+    const bonafied = await Bonafied.findById(req.params.id);
+    if (!bonafied) {
+      return res.status(404).json({ message: "Requested data Not found" });
+    }
+    res.status(200).json(bonafied);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.create = async (req, res) => {
+  try {
+    const newBonafied = new Bonafied(req.body);
+    await newBonafied.save();
+    res.status(201).json(newBonafied);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
